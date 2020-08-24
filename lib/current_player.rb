b2b1ea227d1e735ad578fcd93a121230a4f699ae
Sarah Dunford <sarahbd97@gmail.com>
2020-08-24 20:28:21 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,16 @@
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  end
+end
+
+
 def turn_count(board)
   counter = 0
   board.each do |index|
-    if board[index] == "X" || board[index] == "O"
+    if position_taken?(board, index) == true
       counter += 1
     end
     return counter
