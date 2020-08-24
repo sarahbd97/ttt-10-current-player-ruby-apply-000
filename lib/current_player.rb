3bc9f5a5c506c294d161b8dc41d2f9124156dc49
Sarah Dunford <sarahbd97@gmail.com>
2020-08-24 20:30:49 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   counter = 0
   board.each do |index|
-    if board[index] == "X" || board[index] == "O"
+    if index == "X" || index == "O"
       counter += 1
     end
     return counter
